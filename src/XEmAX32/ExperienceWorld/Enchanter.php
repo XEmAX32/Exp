6a328c4237505b153e5bebf1138dfe6caeaec1f6
XEmAX32 <XEmAX32@gmail.com>
2015-09-15 18:23:53 +0200

    Create Enchanter.php

---
+++ src/XEmAX32/ExperienceWorld/Enchanter.php
@@ -0,0 +1,18 @@
+<?php
+
+      #############################
+      #   Experience By XEmAX32   #
+      #     v1.0 in Development   #
+      #############################
+      
+namespace ExperienceWorld;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\player;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Enchanter extends PluginBase implements Listener{
+
+public function Enchanter(PlayerInteractEvent $e){
