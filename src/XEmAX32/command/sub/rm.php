d5f550b11e2c322c0348526d1c9e82921eda9906
XEmAX32 <XEmAX32@gmail.com>
2015-09-15 17:28:05 +0200

    Create rm.php

---
+++ src/XEmAX32/command/sub/rm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ExperienceWorld\Commands\sub;
+
+use ExperienceWorld\Commands\SubCmd;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+
+class AddExpSubCmd extends SubCmd{
+  
+public function execute(CommandSender $sender, array $args){
+  $name = $e->getPlayer()->getName();
+  $quantity = 
+  if($sender->hasPermission("experience.manage")){
+    $experience->set($name - $quantity);
+    $sender->sendMessage(TextFormat::AQUA . "You have removed ".$quantity TextFormat::AQUA . "to "$name);
+   }else{
+     $sender->sendMessage(TextFormat::RED . "You don't have permissions for run this command");
+   }
+  }
+}
